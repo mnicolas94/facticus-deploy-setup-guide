f1255f49555581216fa5d7106ded1d77dccb188f
mnicolas94 <miguelcok27@gmail.com>
2023-05-27 19:34:32 -0400

    feat: configure scene to test build

---
+++ Assets/Scripts/VariableDisplayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Deploy.Runtime;
+using TMPro;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class VariableDisplayer : MonoBehaviour
+    {
+        [SerializeField] private StringVariable _stringVariable;
+        [SerializeField] private TextMeshProUGUI _text;
+
+        private void Start()
+        {
+            _text.text = _stringVariable.Value;
+        }
+    }
+}
